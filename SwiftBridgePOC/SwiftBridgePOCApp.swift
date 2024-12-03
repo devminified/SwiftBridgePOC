b8f616a2657df3ae9b07ad1a061a7af38bef9a9a
dev-minified <frontendrock@gmail.com>
2024-12-03 17:22:07 +0500

    Initial Commit

---
+++ SwiftBridgePOC/SwiftBridgePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBridgePOCApp.swift
+//  SwiftBridgePOC
+//
+//  Created by Devminified on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBridgePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
